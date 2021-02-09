959c4bab15959c04aca37b625b15e9f872bdddc0
d_melnichenko <d.melnichenko@newagesol.com>
2021-02-09 15:49:36 +0200

    3.4. Заголовок в файле java [#468571]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("09.02.2021");
+    }
+}
